3693663d5775c34f5ac1c93472184b2881af5c1c
Jing Ren <jingren@192.168.1.112>
2023-07-23 13:36:14 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        You have landed on this page because we could not detect which School
+        home page we should send you to. In the first instance, you should find
+        the School home pages relevant to your studies by looking at the menu
+        shown in the 'My Home page' block on the right-hand side of this page.
+        If the page you are looking for is not available on the drop-down menu,
+        please select a landing page by following the instructions in Part 3 of
+        this guide Home Pages in QMPlus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
